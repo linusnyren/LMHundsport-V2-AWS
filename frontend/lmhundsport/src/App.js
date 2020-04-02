6c42ec35cc0e9bd2edb240d12876887ce1228009
linusny@hotmail.com <linusny@hotmail.com>
2020-04-02 23:43:30 +0200

    Setup aws amplify in frontend

--- frontend/lmhundsport/src/App.js
+++ frontend/lmhundsport/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Amplify,{Auth} from 'aws-amplify';
+import AWSAmplifyConfig from './config/AWS-AmplifyConfig';
+import Register from './components/Register'
 function App() {
+  Amplify.configure(AWSAmplifyConfig);
   return (
     <div className="App">
       <header className="App-header">
+        <Register />
         <h1 className="welcome"> Välkommen till LM Hundsport</h1>
       </header>
     </div>
